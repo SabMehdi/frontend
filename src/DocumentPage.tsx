bd121e962e42ddb7ab2d350f918873e35e3b19f6
saber <kadiri112@icloud.com>
2023-11-23 21:27:27 +0100

    add params

--- src/DocumentPage.tsx
+++ src/DocumentPage.tsx
@@ -5,12 +5,16 @@ import axios from 'axios';
 const DocumentPage: React.FC = () => {
   const { id } = useParams<{ id: string }>();
   const [documentContent, setDocumentContent] = useState<string>('');
+  const [title, setTitle] = useState(null)
+  const [path, setPath] = useState(null)
 
   useEffect(() => {
     const fetchDocument = async () => {
       try {
         const response = await axios.get(`http://localhost:8000/api/document/${id}/`);
+        setTitle(response.data.name)
         setDocumentContent(response.data.content);
+        setPath(response.data.path)
       } catch (error) {
         console.error('Error fetching document:', error);
       }
@@ -21,7 +25,9 @@ const DocumentPage: React.FC = () => {
 
   return (
     <div>
-      <h1>Document {id}</h1>
+      <h1>{title}</h1>
+      <h2>{path}</h2>
+
       <div>{documentContent}</div>
     </div>
   );

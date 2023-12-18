f21fd9745d4bc11a36e94eefe8618cdf34628d9a
saber <1999ms@live.fr>
2023-12-18 02:47:00 +0100

    underline search

--- src/TagCloud.tsx
+++ src/TagCloud.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import './FileUpload.css';
+
 interface WordData {
   positions: number[];
   pos: string;
@@ -27,23 +29,23 @@ const TagCloud: React.FC<TagCloudProps> = ({ invertedIndexData }) => {
     <div style={{ display: 'flex', flexWrap: 'wrap', justifyContent: 'center' }}>
       {Object.entries(invertedIndexData).map(([lemma, data]) => (
         <Link
-        to={`/search?q=${encodeURIComponent(data.original)}`}
-        style={{
-          fontSize: `${calculateFontSize(data.positions.length)}px`,
-          color: calculateColor(data.positions.length),
-          margin: '10px',
-          transition: 'transform 0.1s ease',
-          cursor: 'pointer',
-          
-        }}
-        title={`Occurrences: ${data.positions.length}`}
-        onMouseOver={(e) => e.currentTarget.style.transform = 'scale(1.2)'}
-        onMouseOut={(e) => e.currentTarget.style.transform = 'scale(1)'}
-
-        key={lemma}
+          to={`/search?q=${encodeURIComponent(data.original)}`}
+          className="tag-cloud-link" // Add this class
+          style={{
+            fontSize: `${calculateFontSize(data.positions.length)}px`,
+            color: calculateColor(data.positions.length),
+            margin: '10px',
+            transition: 'transform 0.1s ease',
+            cursor: 'pointer',
+          }}
+          title={`Occurrences: ${data.positions.length}`}
+          onMouseOver={(e) => e.currentTarget.style.transform = 'scale(1.2)'}
+          onMouseOut={(e) => e.currentTarget.style.transform = 'scale(1)'}
+          key={lemma}
         >
-        {data.original}
+          {data.original}
         </Link>
+
       ))}
     </div>
   );
